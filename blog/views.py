82f67fb0aad2485dc890fe79538ad6fe7dd159d4
Sandra <sandy_purrple@hotmail.com>
2016-02-27 17:27:55 -0600

    Added views to create/edit blog post inside the side

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 def post_list(request):
 	return render(request, 'blog/post_list.html', {})
 
@@ -9,5 +9,9 @@ from .models import Post
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts':posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
 
 # Create your views here.
